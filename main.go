ac1ee4a500a4af7cdb515190f014b99f46015fad
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-05-19 10:45:29 -0300

    dotenv applied on contexts

--- main.go
+++ main.go
@@ -27,11 +27,11 @@ func main() {
 }
 
 func dataToJson (input string) string {
-	return preMadeQuestion := "Transform the following string data into a organized json objects array with the format being [ { contentName: content, ...} , ...}], identify the content name via the content itself, content: " + input 
+	return preMadeQuestion := os.Getenv("DATA_TO_JSON_CONTEXT") + input 
 }
 
 func dataToExcelFormat (input string) string {
-	return preMadeQuestion := "Transform the following string data into a organized excel format text(to be transformed within excel to sheets), you identify what the first columns will be  by the type of content that the data provides,  content: " + input 
+	return preMadeQuestion := os.Getenv("DATA_TO_EXCEL_CONTEXT") + input 
 }
 
 func postRequest (promptInput string) string {
