7df44930dbfeca5501bff5303392d699f295d926
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-05-19 10:04:12 -0300

    uuid integration

--- app/models/models.schema.go
+++ app/models/models.schema.go
@@ -3,10 +3,12 @@ package models
 import (
 	"time"
 	"gorm.io/gorm"
+	"github.com/google/uuid"
 )
 
 type Convo struct {
 	gorm.Model
+	Id 	     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
 	UserInput    string
 	ModelOutput  string
 	Timestamp    time.Time
