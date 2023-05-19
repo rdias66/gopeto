b56388be99e2b95f48055938dfd7231193451a6d
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-05-19 10:23:16 -0300

    data to json func

--- main.go
+++ main.go
@@ -15,7 +15,34 @@ func main() {
   	if err != nil {
   	 	log.Fatal("Error loading .env file")
   	}
-	
-	openaiClient := openai.NewClient(os.Getenv("YOUR_API_KEY"))
-	// ...
+	prompt := "What is the meaning of life?"
+	conversation := []string{prompt}
+
+	// Prepare the API request payload
+	data := fmt.Sprintf(`{
+		"prompt": "%s",
+		"max_tokens": 50,
+		"temperature": 0.7,
+		"n": 1,
+		"stop": "\n"
+	}`, strings.Join(conversation, "\n"))
+
+	// Make the API request
+	apiKey := os.Getenv("YOUR_API_KEY")
+	url := "https://api.openai.com/v1/engines/davinci-codex/completions"
+	req, _ := http.NewRequest("POST", url, strings.NewReader(data))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	client := &http.Client{}
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+
+	// Parse the API response
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
 }
+
+func dataToJson (input string) string {
+	return preMadeQuestion := "Transform the following string data into a organized json objects array with the format being [ { contentName: content, ...} , ...}], identify the content name via the content itself, content: " + input 
+}
+
